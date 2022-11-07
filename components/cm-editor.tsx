864f75ddc8e6d5408567f47bd4e7de30c13d17e5
aeither <36173828+aeither@users.noreply.github.com>
2022-11-07 16:33:29 +0100

    toggle answer, mutate

--- components/cm-editor.tsx
+++ components/cm-editor.tsx
@@ -1,22 +1,27 @@
-import React, { useCallback, useEffect } from "react";
-import { EditorState } from "@codemirror/state";
-import useCodeMirror from "@/lib/hooks/use-codemirror";
+import React, { useCallback, useEffect, useState } from "react"
+import { EditorState } from "@codemirror/state"
+import useCodeMirror from "@/lib/hooks/use-codemirror"
 
 interface Props {
-  initialDoc: string;
-  onChange: (doc: string) => void;
+  initialDoc: string
+  onChange: (doc: string) => void
 }
 
 const Editor: React.FC<Props> = (props) => {
-  const { onChange, initialDoc } = props;
+  const { onChange, initialDoc } = props
+  const [isAnswer, setIsAnswer] = useState(false)
+
   const handleChange = useCallback(
-    (state: EditorState) => onChange(state.doc.toString()),
+    (state: EditorState) => {
+      onChange(state.doc.toString())
+    },
     [onChange]
-  );
-  const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
-    initialDoc: initialDoc,
-    onChange: handleChange,
-  });
+  )
+  const { refContainer, editorView, mutateText } =
+    useCodeMirror<HTMLDivElement>({
+      initialDoc: initialDoc,
+      onChange: handleChange,
+    })
 
   useEffect(() => {
     if (editorView) {
@@ -24,14 +29,29 @@ const Editor: React.FC<Props> = (props) => {
     } else {
       // loading editor
     }
-  }, [editorView]);
+  }, [editorView])
 
   return (
-    <div
-      className="w-auto h-full flex-grow-0 flex-shrink-0"
-      ref={refContainer}
-    ></div>
-  );
-};
+    <>
+      <div
+        className="w-auto h-full flex-grow-0 flex-shrink-0"
+        ref={refContainer}
+      ></div>
+      <button
+        onClick={() => {
+          if (isAnswer) {
+            setIsAnswer(false)
+            mutateText("# hello from cm-editor \n")
+          } else {
+            setIsAnswer(true)
+            mutateText("# hello from starter \n")
+          }
+        }}
+      >
+        {isAnswer ? "Show Answer" : "Reset"}
+      </button>
+    </>
+  )
+}
 
-export default Editor;
+export default Editor
--- lib/hooks/use-codemirror.ts
+++ lib/hooks/use-codemirror.ts
@@ -1,21 +1,25 @@
-import type React from "react"
-import { useEffect, useState, useRef } from "react"
-import { basicSetup } from "codemirror"
-import { EditorState } from "@codemirror/state"
-import { EditorView, keymap } from "@codemirror/view"
 import { defaultKeymap } from "@codemirror/commands"
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown"
 import { languages } from "@codemirror/language-data"
+import { EditorState } from "@codemirror/state"
 import { oneDark } from "@codemirror/theme-one-dark"
+import { EditorView, keymap } from "@codemirror/view"
+import { basicSetup } from "codemirror"
+import type React from "react"
+import { useEffect, useRef, useState } from "react"
 
 interface Props {
   initialDoc: string
   onChange?: (state: EditorState) => void
 }
 
-const useCodeMirror = <T extends Element>(
-  props: Props
-): [React.MutableRefObject<T | null>, EditorView?] => {
+interface Return<T> {
+  refContainer: React.MutableRefObject<T | null>
+  editorView?: EditorView
+  mutateText?: (text: string) => void
+}
+
+const useCodeMirror = <T extends Element>(props: Props): Return<T> => {
   const refContainer = useRef<T>(null)
   const [editorView, setEditorView] = useState<EditorView>()
   const { onChange } = props
@@ -55,7 +59,21 @@ const useCodeMirror = <T extends Element>(
     }
   }, [refContainer])
 
-  return [refContainer, editorView]
+  const mutateText = (text: string) => {
+    editorView.dispatch({
+      changes: {
+        from: 0,
+        to: editorView.state.doc.toString().length,
+        insert: text,
+      },
+      selection: {
+        anchor: editorView.state.selection.ranges[0].from,
+        head: editorView.state.selection.ranges[0].from,
+      },
+    })
+  }
+
+  return { refContainer, editorView, mutateText }
 }
 
 export default useCodeMirror
